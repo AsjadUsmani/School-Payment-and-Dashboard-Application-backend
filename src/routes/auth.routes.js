5b43acb2a3992581aebcd061a5e7ac0b6ff4f248
Asjad Usmani <asjadusmani4@gmail.com>
2025-09-16 22:38:33 +0530

    Addition

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { register, login, logout } from "../controllers/auth.controller.js";
+import { register, login, logout, me } from "../controllers/auth.controller.js";
 import { validateLogin, validateRegister } from "../middlewares/auth.middleware.js";
 import verifyAuth from "../middlewares/auth.middleware.js";
 
