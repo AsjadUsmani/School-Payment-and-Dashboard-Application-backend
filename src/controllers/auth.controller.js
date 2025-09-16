b58dd73cff9a91a86a9b015f3ae61680fb348d92
Asjad Usmani <asjadusmani4@gmail.com>
2025-09-16 22:19:52 +0530

    fix: update CORS and cookie config

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -24,6 +24,7 @@ export const register = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
       maxAge: 3600000, // 1 hour
     });
 
@@ -50,6 +51,7 @@ export const login = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
       maxAge: 3600000, // 1 hour
     });
 
